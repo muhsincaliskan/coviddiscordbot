b60c21ba773bf7385598f8dffa630e69a8fb1487
killerbean26 <34723829+killerbean26@users.noreply.github.com>
2020-05-12 21:55:30 +0300

    embed message integrate, method rewrite,bug fix

--- src/index.js
+++ src/index.js
@@ -5,72 +5,84 @@ const Discord = require("discord.js")
 const bot = new Discord.Client()
 
 var fs = require("fs")
-let raw = fs.readFileSync("./filter.json")
+let raw = fs.readFileSync("../filter.json")
 let filter = JSON.parse(raw)
 
-let rawReact=fs.readFileSync("./reactions.json")
-let swearReaction=JSON.parse(rawReact)
-var swearCounter=0
+let rawReact = fs.readFileSync("../reactions.json")
+let swearReaction = JSON.parse(rawReact)
+var swearCounter = 0
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`)
-    bot.user.setPresence({ game: { name: 'COVID-19' }, status: 'online' })
+    bot.user.setPresence({ game: { name: 'COVID-19' }, status: 'invisible' })
 })
 bot.on('message', message => {
     message.content = message.content.toLowerCase()
-    if (message.content.startsWith("cov") && message.content.endsWith("cov")) {
+    if (message.content.startsWith("cov") && message.content.length==3) {
         getall(message)
     }
     else if (message.content.startsWith("cov ")) {
-            var arr = message.content.split(" ")
-            var command = arr[1]
-            console.log(arr)
-            var getFilterInfo = search(filter, arr)
-            var isSwear = getFilterInfo.isSwear
-            var Swear = getFilterInfo.Swear
-            
-            if (isSwear) {
-                swearCounter++;
-                console.log(Swear + " deleted")
-                message.delete()
-                if (swearCounter>6&&swearCounter<15) {
-                    var indexofReaction= Math.floor((Math.random() * swearReaction.length) + 0)
-                    message.reply("\n"+swearReaction[indexofReaction])  
-                }
-                else if (swearCounter==15) {
-                    message.channel.send("Sizle uğraşamayacam\nBulaşmam gereken insanlar var.")
-                    swearCounter=0
-                }    
+        var arr = message.content.split(" ")
+        
+        console.log(arr)
+        arr = arr.slice(1)
+        console.log(arr)
+        var getFilterInfo = search(filter, arr)
+        var isSwear = getFilterInfo.isSwear
+        var Swear = getFilterInfo.Swear
+        var command=""
+        for (let index = 0; index < arr.length; index++) {
+                command = command+" "+ arr[index];   
+        }
+        command=command.trimStart()
+        command=command.trimEnd()
+        console.log(command)
+        if (isSwear) {
+            swearCounter++;
+            console.log(Swear + " deleted")
+            message.delete()
+            if (swearCounter > 6 && swearCounter < 15) {
+                var indexofReaction = Math.floor((Math.random() * swearReaction.length) + 0)
+                message.reply("\n" + swearReaction[indexofReaction])
             }
-            else if (message.content == ("cov "+command) && arr.length < 3) {
-                if (command=="top") {
-                    getsorted(message, "cases")
-                } else {
-                    getcountry(message, command)
-                }
+            else if (swearCounter == 15) {
+                message.channel.send("Sizle uğraşamayacam\nBulaşmam gereken insanlar var.")
+                swearCounter = 0
             }
-            else if (message.content.startsWith("cov usa ") && (arr.length > 2 && arr.length < 5)) {
-                command = message.content.substring(8)
+        }
+        else if (message.content == ("cov " + command)) {
+            if (command == "top" || command == "leaderboard") {
+                getsorted(message, "cases")
+            }
+            else if (command == "all") {
+                getall(message)
+            }
+            else if (command == "help") {
+                message.channel.send({ embed: messageTemplate("", true) })
+
+            }
+            else if (command.startsWith("usa ")) {
+                command = command.slice(4)
                 getState(message, command)
             }
             else {
-                message.channel.send("Too much arguments!\nYou can try ISO code.")
+                getcountry(message, command)
             }
+        }
+        else {
+            message.channel.send("Too much arguments!\nYou can try ISO code.")
+        }
     }
 })
 async function getall(message) {
     let all = await covid.all()
-    let allYesterday = await covid.all({ yesterday: true })
-    message.channel.send("Global Stats:\n\n" + messageTemplate(all, allYesterday))
+    message.channel.send({ embed: messageTemplate(all) })
     return 1
 }
 async function getcountry(message, command) {
     let specificCountry = await covid.countries(command)
-    let yesterdayCountry = await covid.countries(command, { yesterday: true })
     if (specificCountry.message === undefined) {
-        specificCountryMessage =
-            "Country: **" + specificCountry.country + "**\n\n" + messageTemplate(specificCountry, yesterdayCountry)
-        message.channel.send(specificCountryMessage)
+        message.channel.send({ embed: messageTemplate(specificCountry) })
     }
     else
         message.channel.send(specificCountry.message + "\nYou can try ISO code.");
@@ -87,22 +99,26 @@ async function getsorted(message, sorttype) {
         else if (sorttype == "recovered")
             top10[index] = index + 1 + ". " + sorteddata[index].country + ": **" + sorteddata[index].recovered + "**\n"
     }
-    message.channel.send("Sorted by *" + sorttype + "*\n" + top10.toString().replace(/,/g, ""))
+    const embedMsg = {
+        color: 0x0099ff,
+        author: {
+            name: 'Top 10 ' + sorttype,
+            icon_url: 'https://i.imgur.com/nP4sNCes.jpg',
+        },
+        description: top10.toString().replace(/,/g, ""),
+        footer: { text: 'cov help for commands' }
+    }
+    message.channel.send({ embed: embedMsg })
 }
 async function getState(message, command) {
     let states = await covid.states(command)
-    let yesterdayStates=await covid.states(command,{yesterday:true})
-    var stateMessage
     if (states.message === undefined) {
-        stateMessage =
-            "State: **" + states.state + "**\n\n" + messageTemplate(states, yesterdayStates)
-        message.channel.send(stateMessage)
+        message.channel.send({ embed: messageTemplate(states) })
     }
     else
         message.channel.send(states.message + "\nYou can try ISO code.");
 
 }
-
 function search(data, word) {
     let Swear = ""
     for (let index = 0; index < data.length; index++) {
@@ -115,15 +131,85 @@ function search(data, word) {
     }
     return { isSwear: false, Swear: Swear }
 }
-function messageTemplate(data, yesterdayData = "") {
-    var msg
-    msg =
-    "Cases: **" + yesterdayData.cases + " ➡️ " + data.cases + " 🔺" + (data.cases - yesterdayData.cases) + "**\n\n" +
-    "Deaths: **" + yesterdayData.deaths + " ➡️ " + data.deaths + " 🔺" + (data.deaths - yesterdayData.deaths) + "**\n\n" 
-    if (data.state == undefined) {
-        msg = msg +"Recovered: **" + yesterdayData.recovered + " ➡️ " + data.recovered + " 🔺" + (data.recovered - yesterdayData.recovered) + "**"
-    }
-    return msg
-}
+function messageTemplate(data = "", help = false) {
+    const embedMsg = {
+        color: 0x0099ff,
+        author: {
+            name: '',
+            icon_url: 'https://i.imgur.com/nP4sNCes.jpg',
+        },
+        description: '',
+        thumbnail: {
+            url: "",
+        },
+        fields: [
 
+        ],
+        footer: { text: '`cov help` for commands' }
+    };
+    if (help) {
+        embedMsg.author.name = "Commands"
+        embedMsg.description =
+            "GET TOTAL DATA: `cov` or `cov all`\n\n" +
+            "GET SPECIFIC COUNTRY DATA: `cov country name`,\n `cov country iso2` or `iso3` code\n" +
+            "Example: `cov Turkey`, `cov tr`, `cov tur`\n\n" +
+            "GET TOP 10 CASE: `cov top`, `cov leaderboard`\n" +
+            "Get Commands: `cov help`\n\n" +
+            "This bot created by killerbean and BOT uses NovelCovid API"
+    }
+    else {
+        embedMsg.fields = [{
+            name: "Cases",
+            value: data.cases,
+            inline: true
+        },
+        {
+            name: 'Active',
+            value: data.active,
+            inline: true,
+        },
+        {
+            name: 'Recovered',
+            value: data.recovered,
+            inline: true,
+        },
+        {
+            name: 'Critical',
+            value: data.critical,
+            inline: true,
+        },
+        {
+            name: 'Deaths',
+            value: data.deaths,
+            inline: true,
+        },
+        {
+            name: 'Tests',
+            value: data.tests,
+            inline: true,
+        },
+        {
+            name: 'Cases Today',
+            value: data.todayCases,
+            inline: true,
+        },
+        {
+            name: 'Deaths Today',
+            value: data.todayDeaths,
+            inline: true,
+        }]
+        if (data.country != undefined) {
+            embedMsg.thumbnail.url = data.countryInfo.flag
+            embedMsg.author.name = "COVID-19 Statistics for " + data.country + " (" + data.countryInfo.iso2 + ")"
+        }
+        else if (data.country == undefined && data.state == undefined) {
+            embedMsg.author.name = "COVID-19 Total Data for " + data.affectedCountries + " countries"
+        }
+        else if (data.state != undefined) {
+            embedMsg.author.name = "COVID-19 Statistics for " + data.state
+            embedMsg.fields.splice(2, 2)
+        }
+    }
+    return embedMsg
+}
 bot.login(process.env.BOT_TOKEN)
